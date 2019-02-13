9050f57900bc6e43baaef25c7f600f71ad5647b5
Kyle Chung <kchung1068@jh-mba158-04.local>
2019-02-13 14:31:43 -0600

    added Items

---
+++ Shopping List/Item.swift
@@ -0,0 +1,38 @@
+//
+//  Item.swift
+//  Shopping List
+//
+//  Created by Kyle Chung on 2/12/19.
+//  Copyright © 2019 Kyle Chung. All rights reserved.
+//
+
+import Foundation
+class item {
+    
+    var name: String
+    init(name:String) {
+        self.name = name
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -11,12 +11,35 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")! but can crash
         
-        return
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") {
+            
+            cell.textLabel?.text = "Hello"
+            
+            
+            if indexPath.row % 2 == 0 {
+            cell.backgroundColor = UIColor.blue
+            cell.textLabel?.textColor = UIColor.orange
+            } else {
+                cell.backgroundColor = UIColor.yellow
+                cell.textLabel?.textColor = UIColor.purple
+                
+            }
+            
+            return cell
+        } else {
+            
+        return UITableViewCell()
+            
+        }
+        
+        
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 10
+        return 19
     }
     
 
@@ -24,9 +47,15 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet var newItemTextField: UITextField!
     
+    var items: [Item] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        
+        let itemOne = Item(name: "Milk")
+        let itemTwo = Item(name: "Blueberries")
+        items = [itemOne, itemTwo]
     }
 
     @IBAction func itembarButtonPressed(_ sender: Any) {
